52d445f00b88a967747098cd82a6f0702165505b
JE-Wardrop <3110drop@gmail.com>
2026-03-16 19:58:56 +1100

    added things to js again

--- index.js
+++ index.js
@@ -4,3 +4,10 @@ function addNumbers(a, b){
 
 const result = addNumbers(5, 3);
 console.log(result);
+
+
+setTimeout(() => {
+    console.log("This runs after 2 seconds");
+}, 2000);
+
+console.log("end")
