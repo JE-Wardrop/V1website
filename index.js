e8a33ccf2bd717402fc338de14b040c899d07ad0
JE-Wardrop <3110drop@gmail.com>
2026-06-10 20:50:37 +1000

    Changes to page, still trying to work out a good layout that I like

--- index.js
+++ index.js
@@ -1,28 +1,2 @@
-const checkPalbut = document.getElementById("inputPalindrome");
-checkPalbut.addEventListener("click", checkPalindrome);
 
-const checkSepCipherbut = document.getElementById("inputSepCipher");
-checkSepCipherbut.addEventListener("click", checkSepChipher);
-
-function reverseString(str) {
-    return str.split("").reverse().join("");
-}
-
-function checkPalindrome(){
-    // const text = inputPalindrome.value;
-    // const reversed = reverseString(text);
-
-    // if (text === reversed) {
-    //     document.getElementById("displayPali").innerHTML = "It is a palindrome!";
-    // } else {
-    //     document.getElementById("displayPali").innerHTML = "Not a palindrome.";
-    // }
-
-    document.getElementById("displayPali").innerHTML = "Not working function yet";
-}
-
-
-function checkSepChipher(){
-
-}
 
